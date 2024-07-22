a677eb8aecf43a8c6980f19776dbb3b8017cfda7
Mondherjendoubi <mondher.jendoubi.98@gmail.com>
2024-07-22 13:08:54 +0200

    Update nuxt.js

--- nuxt.js
+++ nuxt.js
@@ -15,5 +15,14 @@ export default defineNuxtModule({
       // Add ./composables directory to the list
       dirs.push(resolve("./composables"));
     },
+    "autoImports:sources": (sources) => {
+      const { resolve } = createResolver(import.meta.url);
+      // Ensure composables can be imported directly
+      sources.push({
+        from: resolve("./composables/useIcons"),
+        name: "useIcons",
+        as: "useIcons",
+      });
+    },
   },
 });
